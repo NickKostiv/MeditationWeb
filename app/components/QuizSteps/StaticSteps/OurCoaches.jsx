aab7876fd2f003fedc127684f2396b6c5dc89981
swipe <swipe@MacBook-Air-SWIPE-2.local>
2025-01-08 14:07:36 +0200

    added 17,18 pages

--- app/components/QuizSteps/StaticSteps/OurCoaches.jsx
+++ app/components/QuizSteps/StaticSteps/OurCoaches.jsx
@@ -1,15 +1,69 @@
 import React from "react";
+import Image from "next/image";
+import coachesImg from "../../../../public/coaches__people.webp";
 
 export default function OurCoaches({ onContinue }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-2xl font-bold mb-4">Our Coaches</h1>
-      <p className="mb-8">Learn more about our amazing coaches...</p>
-      <button
-        onClick={onContinue}
-        className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition">
-        Continue
-      </button>
+    <div className="flex w-full max-w-[550px] flex-col h-full overflow-hidden min-h-screen items-center justify-between mt-[80px] bg-transparent  bg-gray-900 text-white">
+      <h2
+        className="text-[22px] font-bold leading-[24px] text-center mb-10 px-[25px]"
+        style={{
+          backgroundImage:
+            "linear-gradient(98deg, #fff 34.56%, #27e2ef 79.62%)",
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+          backgroundClip: "text",
+        }}>
+        It took us 6 years, 100+ coaches and therapists to create our ultimate
+        program for chakra balancing
+      </h2>
+
+      {/* Moving Image Section */}
+      <div className="w-full h-[300px] overflow-hidden">
+        <div
+          style={{
+            display: "flex",
+            animation: "moveSlider 40s linear infinite",
+            animationTimingFunction: "linear",
+            width: "200%", // Ширина контейнера для двох копій зображення
+          }}>
+          <Image
+            src={coachesImg}
+            alt="Coaches image"
+            className=""
+            style={{
+              objectFit: "cover",
+              width: "100%", // Зображення займає половину контейнера
+            }}
+          />
+          <Image
+            src={coachesImg} // Дублюємо зображення
+            alt="Coaches image"
+            style={{
+              objectFit: "cover",
+              width: "100%", // Зображення займає другу половину
+            }}
+          />
+        </div>
+        <style jsx>{`
+          @keyframes moveSlider {
+            0% {
+              transform: translateX(0%);
+            }
+            100% {
+              transform: translateX(-50%); /* Рух на половину контейнера */
+            }
+          }
+        `}</style>
+      </div>
+
+      <div className="w-full max-w-[550px] px-[10px] mb-[120px]">
+        <button
+          className="relative w-full max-w-[550px] h-[44px] inline-flex mx-auto items-center justify-center bg-[#2fd1db] text-white text-[17px] font-semibold uppercase leading-[28px] py-4 px-5 rounded-[64px] shadow-none transition-all duration-300 ease-in-out hover:bg-blue-600 focus:outline-none active:bg-blue-700"
+          onClick={onContinue}>
+          Continue
+        </button>
+      </div>
     </div>
   );
 }
--- app/components/QuizSteps/StaticSteps/OurMethods.jsx
+++ app/components/QuizSteps/StaticSteps/OurMethods.jsx
@@ -1,15 +1,45 @@
 import React from "react";
+import Image from "next/image";
+import coachesImg from "../../../../public/university-logo.png";
 
-export default function OurMethods({ onContinue }) {
+export default function OurCoaches({ onContinue }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-2xl font-bold mb-4">Our Methods</h1>
-      <p className="mb-8">Discover our unique approach and methods...</p>
-      <button
-        onClick={onContinue}
-        className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition">
-        Continue
-      </button>
+    <div className="flex w-full max-w-[550px] flex-col h-full overflow-hidden min-h-screen items-center justify-between mt-[80px] bg-transparent  bg-gray-900 text-white">
+      <h2
+        className="text-[28px] mt-[30px] text-center font-bold leading-[100%] uppercase mb-10 px-[25px]"
+        style={{
+          backgroundImage:
+            "linear-gradient(98deg, #fff 34.56%, #27e2ef 79.62%)",
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+          backgroundClip: "text",
+        }}>
+        Get ready <br />
+        for the real <br />
+        life changes
+      </h2>
+
+      <Image
+        src={coachesImg}
+        alt="Coaches image"
+        className="px-[20px]"
+        style={{
+          objectFit: "cover",
+          width: "100%", // Зображення займає половину контейнера
+        }}
+      />
+
+      <p className="mt-[20px] text-[18px] font-light text-center text-white">
+        The methods we use are so <br /> effective, they are recommended <br />
+        <strong> all around the world</strong>
+      </p>
+      <div className="w-full max-w-[550px] px-[10px] mb-[120px]">
+        <button
+          className="relative w-full max-w-[550px] h-[44px] inline-flex mx-auto items-center justify-center bg-[#2fd1db] text-white text-[17px] font-semibold uppercase leading-[28px] py-4 px-5 rounded-[64px] shadow-none transition-all duration-300 ease-in-out hover:bg-blue-600 focus:outline-none active:bg-blue-700"
+          onClick={onContinue}>
+          Continue
+        </button>
+      </div>
     </div>
   );
 }
