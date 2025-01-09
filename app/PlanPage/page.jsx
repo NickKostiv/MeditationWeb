cdabdf1ee264244839009aa8cd69e5938107a35b
swipe <swipe@MacBook-Air-SWIPE-2.local>
2025-01-09 18:30:26 +0200

    plan page fixed

--- app/PlanPage/page.jsx
+++ app/PlanPage/page.jsx
@@ -26,8 +26,8 @@ import planFooterImg from "../../public/plan__footer.webp";
 
 export default function PlanPage({ onContinue }) {
   return (
-    <div className="">
-      <div className="relative bg-[radial-gradient(77.28%_53.46%_at_80.97%_37.09%,rgb(12,38,18)_0%,rgb(8,1,19)_100%)] flex flex-col items-center  md:justify-start  text-white">
+    <div className="relative bg-[radial-gradient(77.28%_53.46%_at_80.97%_37.09%,rgb(12,38,18)_0%,rgb(8,1,19)_100%)]">
+      <div className="overflow-hidden flex flex-col items-center  md:justify-start  text-white">
         <div className="w-full max-w-[550px]">
           <HeaderAnalyze />
           <div className="mt-[90px] flex-col px-6 items-center justify-between w-full">
@@ -183,167 +183,166 @@ export default function PlanPage({ onContinue }) {
             </div>
           </div>
         </div>
-        <div className="pt-[26px]  px-[20px] flex flex-col items-center  md:justify-start bg-[radial-gradient(77.28%_53.46%_at_80.97%_37.09%,rgb(12,38,18)_0%,rgb(8,1,19)_100%)]">
-          <div className="w-full max-w-[550px]">
-            <Image
-              src={planEditorsChoiceImg}
-              alt="PlanEditroImg"
-              width={540}
-              height={140}
-            />
-            <div className="flex justify-center gap-[35px] md:gap-[150px] items-center font-sans text-white w-full mt-[30px]">
-              <div className="flex flex-col items-center text-center">
-                <span className="text-[40px] font-bold">1000+</span>
-                <p className="text-[14px] font-normal tracking-[-0.02px] leading-[1.5]">
-                  Tracks to use <br /> in your <strong>daily</strong> <br />{" "}
-                  practice
-                </p>
-              </div>
-              <div className="flex flex-col items-center text-center">
-                <span className="text-[40px] font-bold">100+</span>
-                <p className="text-[14px] font-normal tracking-[-0.02px] leading-[1.5]">
-                  Therapists and <br /> coaches to <br /> <strong>guide</strong>{" "}
-                  you
-                </p>
-              </div>
+      </div>
+      <div className="pt-[26px]  px-[20px] flex flex-col items-center  md:justify-start bg-[radial-gradient(77.28%_53.46%_at_80.97%_37.09%,rgb(12,38,18)_0%,rgb(8,1,19)_100%)]">
+        <div className="w-full max-w-[550px]">
+          <Image
+            src={planEditorsChoiceImg}
+            alt="PlanEditroImg"
+            width={540}
+            height={140}
+          />
+          <div className="flex justify-center gap-[35px] md:gap-[150px] items-center font-sans text-white w-full mt-[30px]">
+            <div className="flex flex-col items-center text-center">
+              <span className="text-[40px] font-bold">1000+</span>
+              <p className="text-[14px] font-normal tracking-[-0.02px] leading-[1.5]">
+                Tracks to use <br /> in your <strong>daily</strong> <br />{" "}
+                practice
+              </p>
             </div>
-          </div>
-          <div className="flex-col w-full max-w-[550px] mt-[40px] font-montserrat">
-            <div className="flex">
-              <div className="min-w-[86px] h-[90px] max-h-[90px]">
-                <Image
-                  src={planChoiceImg1}
-                  width={86}
-                  height={90}
-                  alt={planChoiceImg1}
-                />
-              </div>
-              <div className="flex-col  ml-[20px] text-white my-auto">
-                <p className="text-[24px] font-semibold leading-[24px] mb-[13px]">
-                  Huge collection of healing sounds
-                </p>
-                <p className="text-[15px] md:text-[18px]  font-thin leading-[18px]">
-                  Discover a vast array of sounds and frequencies for all chakra
-                  systems
-                </p>
-              </div>
-            </div>
-            <div className="flex mt-[30px]">
-              <div className="min-w-[86px] h-[90px] max-h-[90px]">
-                <Image
-                  src={planChoiceImg2}
-                  width={86}
-                  height={90}
-                  alt={planChoiceImg2}
-                />
-              </div>
-              <div className="flex-col  ml-[20px] text-white my-auto">
-                <p className=" text-[24px] font-semibold leading-[24px] mb-[13px]">
-                  200+ guided meditations
-                </p>
-                <p className="text-[15px] md:text-[18px]  font-thin leading-[18px]">
-                  These meditations are an excellent starting point for
-                  beginners
-                </p>
-              </div>
-            </div>
-            <div className="flex mt-[30px]">
-              <div className="min-w-[86px] h-[90px] max-h-[90px]">
-                <Image
-                  src={planChoiceImg3}
-                  width={86}
-                  height={90}
-                  alt={planChoiceImg3}
-                />
-              </div>
-              <div className="flex-col  ml-[20px] text-white my-auto">
-                <p className=" text-[24px] font-semibold leading-[24px] mb-[13px]">
-                  700+ mantras and shamanic music
-                </p>
-                <p className="text-[15px] md:text-[18px]  font-thin leading-[18px]">
-                  Explore a rich collection of powerful mantras designed to help
-                  you find inner peace
-                </p>
-              </div>
-            </div>
-            <div className="flex mt-[30px]">
-              <div className="min-w-[86px] h-[90px] max-h-[90px]">
-                <Image
-                  src={planChoiceImg4}
-                  width={86}
-                  height={90}
-                  alt={planChoiceImg4}
-                />
-              </div>
-              <div className="flex-col  ml-[20px] text-white my-auto">
-                <p className=" text-[24px] font-semibold leading-[24px] mb-[13px]">
-                  Breathing exercises and pranayama
-                </p>
-                <p className="text-[15px] md:text-[18px]  font-thin leading-[18px]">
-                  Breathwork can reduce stress, anxiety, fatigue, can improve
-                  lung function, focus, and sleep
-                </p>
-              </div>
-            </div>
-            <div className="flex mt-[30px]">
-              <div className="min-w-[86px] h-[90px] max-h-[90px]">
-                <Image
-                  src={planChoiceImg5}
-                  width={86}
-                  height={90}
-                  alt={planChoiceImg5}
-                />
-              </div>
-              <div className="flex-col  ml-[20px] text-white my-auto">
-                <p className=" text-[24px] font-semibold leading-[24px] mb-[13px]">
-                  Personalised meditation plan
-                </p>
-                <p className="text-[15px] md:text-[18px]  font-thin leading-[18px]">
-                  A customized program with a combination of guided meditations,
-                  mantras & sounds
-                </p>
-              </div>
+            <div className="flex flex-col items-center text-center">
+              <span className="text-[40px] font-bold">100+</span>
+              <p className="text-[14px] font-normal tracking-[-0.02px] leading-[1.5]">
+                Therapists and <br /> coaches to <br /> <strong>guide</strong>{" "}
+                you
+              </p>
             </div>
           </div>
         </div>
-        <div className="bg-[radial-gradient(77.28%_53.46%_at_80.97%_37.09%,rgb(12,38,18)_0%,rgb(8,1,19)_100%)] px-[20px] pb-[0px]">
-          <div className="flex-col mx-auto pt-[40px] w-full max-w-[550px]">
-            <Image
-              src={planFooterImg}
-              width={540}
-              height={145}
-              alt="apps footer"
-            />
+        <div className="flex-col w-full max-w-[550px] mt-[40px] font-montserrat">
+          <div className="flex">
+            <div className="min-w-[86px] h-[90px] max-h-[90px]">
+              <Image
+                src={planChoiceImg1}
+                width={86}
+                height={90}
+                alt={planChoiceImg1}
+              />
+            </div>
+            <div className="flex-col  ml-[20px] text-white my-auto">
+              <p className="text-[24px] font-semibold leading-[24px] mb-[13px]">
+                Huge collection of healing sounds
+              </p>
+              <p className="text-[15px] md:text-[18px]  font-thin leading-[18px]">
+                Discover a vast array of sounds and frequencies for all chakra
+                systems
+              </p>
+            </div>
+          </div>
+          <div className="flex mt-[30px]">
+            <div className="min-w-[86px] h-[90px] max-h-[90px]">
+              <Image
+                src={planChoiceImg2}
+                width={86}
+                height={90}
+                alt={planChoiceImg2}
+              />
+            </div>
+            <div className="flex-col  ml-[20px] text-white my-auto">
+              <p className=" text-[24px] font-semibold leading-[24px] mb-[13px]">
+                200+ guided meditations
+              </p>
+              <p className="text-[15px] md:text-[18px]  font-thin leading-[18px]">
+                These meditations are an excellent starting point for beginners
+              </p>
+            </div>
+          </div>
+          <div className="flex mt-[30px]">
+            <div className="min-w-[86px] h-[90px] max-h-[90px]">
+              <Image
+                src={planChoiceImg3}
+                width={86}
+                height={90}
+                alt={planChoiceImg3}
+              />
+            </div>
+            <div className="flex-col  ml-[20px] text-white my-auto">
+              <p className=" text-[24px] font-semibold leading-[24px] mb-[13px]">
+                700+ mantras and shamanic music
+              </p>
+              <p className="text-[15px] md:text-[18px]  font-thin leading-[18px]">
+                Explore a rich collection of powerful mantras designed to help
+                you find inner peace
+              </p>
+            </div>
+          </div>
+          <div className="flex mt-[30px]">
+            <div className="min-w-[86px] h-[90px] max-h-[90px]">
+              <Image
+                src={planChoiceImg4}
+                width={86}
+                height={90}
+                alt={planChoiceImg4}
+              />
+            </div>
+            <div className="flex-col  ml-[20px] text-white my-auto">
+              <p className=" text-[24px] font-semibold leading-[24px] mb-[13px]">
+                Breathing exercises and pranayama
+              </p>
+              <p className="text-[15px] md:text-[18px]  font-thin leading-[18px]">
+                Breathwork can reduce stress, anxiety, fatigue, can improve lung
+                function, focus, and sleep
+              </p>
+            </div>
+          </div>
+          <div className="flex mt-[30px]">
+            <div className="min-w-[86px] h-[90px] max-h-[90px]">
+              <Image
+                src={planChoiceImg5}
+                width={86}
+                height={90}
+                alt={planChoiceImg5}
+              />
+            </div>
+            <div className="flex-col  ml-[20px] text-white my-auto">
+              <p className=" text-[24px] font-semibold leading-[24px] mb-[13px]">
+                Personalised meditation plan
+              </p>
+              <p className="text-[15px] md:text-[18px]  font-thin leading-[18px]">
+                A customized program with a combination of guided meditations,
+                mantras & sounds
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[radial-gradient(77.28%_53.46%_at_80.97%_37.09%,rgb(12,38,18)_0%,rgb(8,1,19)_100%)] px-[20px] pb-[40px]">
+        <div className="flex-col mx-auto pt-[40px] w-full max-w-[550px]">
+          <Image
+            src={planFooterImg}
+            width={540}
+            height={145}
+            alt="apps footer"
+          />
 
-            <div className="flex justify-between text-white mt-[20px]">
-              <div className="flex flex-col items-center text-center">
-                <span className="text-[22px] font-bold">30.000.000+</span>
-                <p className="text-[14px] font-normal tracking-[-0.02px] leading-[1.5]">
-                  Downloads
-                </p>
-              </div>
-              <div className="flex flex-col items-center text-center">
-                <span className="text-[22px] font-bold">4.8</span>
-                <p className="text-[14px] font-normal tracking-[-0.02px] leading-[1.5]">
-                  Rating
-                </p>
-              </div>
-              <div className="flex flex-col items-center text-center">
-                <span className="text-[22px] font-bold">172</span>
-                <p className="text-[14px] font-normal tracking-[-0.02px] leading-[1.5]">
-                  Countries
-                </p>
-              </div>
+          <div className="flex justify-between text-white mt-[20px]">
+            <div className="flex flex-col items-center text-center">
+              <span className="text-[22px] font-bold">30.000.000+</span>
+              <p className="text-[14px] font-normal tracking-[-0.02px] leading-[1.5]">
+                Downloads
+              </p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <span className="text-[22px] font-bold">4.8</span>
+              <p className="text-[14px] font-normal tracking-[-0.02px] leading-[1.5]">
+                Rating
+              </p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <span className="text-[22px] font-bold">172</span>
+              <p className="text-[14px] font-normal tracking-[-0.02px] leading-[1.5]">
+                Countries
+              </p>
             </div>
           </div>
         </div>
-        <div className="w-full max-w-[550px] bg-[#4646464d] sticky bottom-0 left-0 right-0 z-30 text-right p-4 rounded-t-[30px]">
-          <button
-            className="relative w-full inline-flex mx-auto items-center justify-center bg-[#2fd1db] text-white text-[20px] font-semibold uppercase leading-[18px] py-[16px] px-5 rounded-[64px] shadow-none transition-transform duration-300 ease-in-out focus:outline-none active:scale-90"
-            onClick={onContinue}>
-            CONTINUE
-          </button>
-        </div>{" "}
+      </div>
+      <div className="bg-[#4646464d] mx-auto w-[full] max-w-[550px] sticky bottom-0 left-0 right-0 z-30 text-right p-4 rounded-t-[30px]">
+        <button
+          className="relative  inline-flex mx-auto w-full max-w-[550px] items-center justify-center bg-[#2fd1db] text-white text-[22px] font-semibold uppercase leading-[18px] py-[16px] px-5 rounded-[64px] shadow-none transition-all duration-300 ease-in-out hover:bg-blue-600 focus:outline-none active:bg-blue-700"
+          onClick={onContinue}>
+          CONTINUE
+        </button>
       </div>
     </div>
   );
